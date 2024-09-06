a5716ff7fc3bae2bcde1356422a451c185b3d00f
Erik Røed <post@erikroed.no>
2024-09-06 23:43:51 +0200

    Remove debug line from build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -3,6 +3,4 @@
 read -p "Input username for the container: " USERNAME
 read -sp "Input password for the user: " SUDO_PASS
 
-echo "Building image with the following input: $USERNAME and $SUDO_PASS"
-
 DOCKER_BUILDKIT=1 docker build --build-arg USERNAME=${USERNAME} --build-arg SUDO_PASS=${SUDO_PASS} -t new-computer .
